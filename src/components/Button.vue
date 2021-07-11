a73ef6cdb99c60736dbc8e6a09d34875e641847d
5hadown1 <shadownight@inbox.ru>
2021-07-11 23:55:21 +0700

    Add homework Lesson3

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<button @click="onClick">ADD NEW COST+</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Button",
+		data() {
+			return {
+			}
+		},
+		methods: {
+			onClick() {
+				this.$emit('clicked');
+			}
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
